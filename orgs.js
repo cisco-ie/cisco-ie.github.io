172de2ad5b52cb464a2caecd61ed8736ec7cab8a
ziausyed <ziausyed@cisco.com>
2017-03-06 10:30:24 -0800

    Mission Projects

--- orgs.js
+++ orgs.js
@@ -1,10 +1,5 @@
 
     var orgs = [
-	{"name": "ziausyed/grpc-java",
-         "type": "repo"},
-        {"name": "HarmonyNFV",
-         "type": "org",
-	 "link": "https://github.com/HarmonyNFV"},
         {"name": "cisco-ie",
 	  "type": "org"},
     ];
